c8eb5502edf969baa79f082eb72254d01fa29298
Peter Daly <peterd060397@gmail.com>
2018-11-20 12:56:45 +0000

    Finished component 3

--- src/input.js
+++ src/input.js
@@ -1,118 +1,110 @@
-
+/*
+ * Peter Daly
+ * Component to allow for easier processing of user input
+ * Days worked on: 30/10/18 - 20/11/18
+ *
+ */
 
 class Input {
   constructor() {
-    this.key = null;
+    this.keys = [];
     this.x = 0;
     this.y = 0;
     this.mouseDirection = null;
     this.gamepads = [];
-
-
+    this.keyHandlers = [];
+    this.buttonsPressed = [];
 
     var that = this;
 
     window.addEventListener("keydown", this.keyDownHandler.bind(null, that));
+    window.addEventListener("keyup", this.keyUpHandler.bind(null, that));
+
     window.addEventListener("mousemove", this.mousePos.bind(null, that));
+
     window.addEventListener("gamepadconnected",
     this.controllerConnect.bind(null, that, true));
     window.addEventListener("gamepaddisconnected",
     this.controllerConnect.bind(null, that, false));
   }
 
-  buttonPressed(b)
-  {
-    if (typeof(b) == "object") {
-      return b.pressed;
-    }
-    return b == 1.0;
+  addUpdateLoop(name, loop) {
+    setInterval(name, loop);
   }
 
-  update()
-  {
+  addKeyHandler(name) {
+    this.keyHandlers.push(name);
+  }
+
+  update() {
     this.gamepads = navigator.getGamepads();
     if (!this.gamepads) {
       return;
     }
     var pad;
+    var pair;
     for(pad of this.gamepads) {
       if(pad) {
-        if(pad.buttons[0].pressed) {
-          console.log("A");
-        }
-        if(pad.buttons[1].pressed) {
-          console.log("B");
-        }
-        if(pad.buttons[2].pressed) {
-          console.log("X");
-        }
-        if(pad.buttons[3].pressed) {
-          console.log("Y");
-        }
-        if(pad.buttons[4].pressed) {
-          console.log("LB");
-        }
-        if(pad.buttons[5].pressed) {
-          console.log("RB");
-        }
-        if(pad.buttons[6].pressed) {
-          console.log("LT");
-        }
-        if(pad.buttons[7].pressed) {
-          console.log("RT");
-        }
-        if(pad.buttons[8].pressed) {
-          console.log("Back");
-        }
-        if(pad.buttons[9].pressed) {
-          console.log("Start");
-        }
-        if(pad.buttons[10].pressed) {
-          console.log("Left Thumbstick");
-        }
-        if(pad.buttons[11].pressed) {
-          console.log("Right Thumbstick");
-        }
-        if(pad.buttons[12].pressed) {
-          console.log("Up");
-        }
-        if(pad.buttons[13].pressed) {
-          console.log("Down");
-        }
-        if(pad.buttons[14].pressed) {
-          console.log("Left");
-        }
-        if(pad.buttons[15].pressed) {
-          console.log("Right");
+        for(var i = 0; i < 16; i++) {
+          pair = [pad.index, i];
+          if(pad.buttons[i].pressed > 0.9 && this.includes(this.buttonsPressed, pair) == -1) {
+            this.buttonsPressed.push([pad.index, i]);
+          }
+          else {
+            var index = this.includes(this.buttonsPressed, pair);
+            if (index > -1) {
+              this.buttonsPressed.splice(index, 1);
+            }
+          }
         }
       }
+
     }
+      return this.buttonsPressed;
   }
 
+  includes(arr1, arr2) {
+    var index = 0;
+    var retIndex = -1;
+    arr1.forEach(function(element) {
+      if(element[0] == arr2[0] && element[1] == arr2[1]) {
+        retIndex = index;
+      }
+      index++;
+    });
 
+    return retIndex;
+  }
 
-  controllerConnect (that, connecting, e)
-  {
+  controllerConnect (that, connecting, e) {
     var gamepad = event.gamepad;
 
     if (connecting) {
       console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
       gamepad.index, gamepad.id,
       gamepad.buttons.length, gamepad.axes.length);
-      that.gamepads[gamepad.index] = gamepad;
     } else {
       console.log("Gamepad disconnected at index %d: %s. %d buttons, %d axes.",
       gamepad.index, gamepad.id,
       gamepad.buttons.length, gamepad.axes.length);
-      delete that.gamepads[gamepad.index];
     }
 
   }
 
-  keyDownHandler (that, e)
-  {
-    that.key = e.key;
-    getKeys(that.key);
+  keyDownHandler (that, e) {
+    if(!that.keys.includes(e.key)) {
+      that.keys.push(e.key);
+    }
+    that.keyHandlers.forEach(function(element) {
+      element(that.keys);
+    });
+  }
+
+  keyUpHandler (that, e) {
+    var index = that.keys.indexOf(e.key);
+    if (index > -1) {
+      that.keys.splice(index, 1);
+    }
   }
 
   mousePos (that, e)
